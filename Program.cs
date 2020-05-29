a9950760d9e1e3cec275b108fa7bf9c74c5fb8e9
Victor Wiedemann <victor.wiedemann@gmail.com>
2020-05-29 09:40:01 -0700

    adding solution file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrackingCodeInterview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
